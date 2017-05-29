403a089452af70e8d86f7eef8210707c74569e38
ChavezE <chaaveze@gmail.com>
2017-05-29 13:47:02 -0500

    Creating algorithm tester

---
+++ Raspberrypi/test_algorithm.py
@@ -0,0 +1,24 @@
+'''
+	Driver to test the algorithm that find the cow
+'''
+
+# importing external libs
+import cv2
+import numpy as np
+import serial
+import time
+from copy import deepcopy
+import math
+import random
+# imporging Roborregos libs
+import Larc_vision_2016 as rb
+
+
+
+
+'''
+    MAIN
+'''
+if __name__ == "__main__":
+	pass
+    
