0568f5155f7fbafaf7a029b7da6a9138fa336125
ChavezE <chaaveze@gmail.com>
2017-10-22 04:04:48 -0500

    ading start buttons

--- Raspberrypi/Pruebas/pruebaBoton.py
+++ Raspberrypi/Pruebas/pruebaBoton.py
@@ -8,20 +8,20 @@ GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(21, GPIO.OUT)
 
-# setting up interrupt on pin 23 to exit the abort the program
+# setting up interrupt on pin 3 to exit the abort the program
 def my_callback(channel):
     print "exiting the program..."
     GPIO.cleanup()
     sys.exit()
 # this line waits for the interrupt and calles the function above
-GPIO.add_event_detect(23, GPIO.FALLING, callback=my_callback, bouncetime=1000)
+GPIO.add_event_detect(3, GPIO.FALLING, callback=my_callback, bouncetime=1000)
 
 # loop in this function until begin button is pressed
 def waitToBegin():
     print "waiting for input"
-    GPIO.output(25,1)
+    GPIO.output(21,1)
     GPIO.wait_for_edge(24, GPIO.FALLING)
-    GPIO.output(25,0)
+    GPIO.output(21,0)
     print "begining program"
 
 
