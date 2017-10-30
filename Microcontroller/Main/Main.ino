eb3834e59e9bd7abcba86f52c1a45b6a3d35672d
Diego Cardozo <diego_a_cardozo@hotmail.com>
2017-10-29 22:10:14 -0600

    Changed name of constPCorrectN to constPCorrect

--- Microcontroller/Main/Main.ino
+++ Microcontroller/Main/Main.ino
@@ -123,8 +123,8 @@ const byte pinUSC_E = 18;
 //P for turns (it is multiplied)
 const int constPTurn = 1;
 
-//P correction Nestor
-const double constPCorrectN = 0.06;
+//P for forward and backward
+const double constPCorrect = 0.06;
 
 //Const for encoder. Perfect at 60cm, 30cm fail by -1.5cm, 100cm by 2cm, 150cm by 3cm
 const long encoder30Cm = 4300L; // TODO: Better it depends in the distance.
