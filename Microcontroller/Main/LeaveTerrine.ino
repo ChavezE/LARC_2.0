988ef852c594b928eb0115261f3efb337772b7e6
Diego Cardozo <diego_a_cardozo@hotmail.com>
2017-11-06 19:05:14 -0600

    Added a first working version of leaveTerrine

--- Microcontroller/Main/LeaveTerrine.ino
+++ Microcontroller/Main/LeaveTerrine.ino
@@ -13,9 +13,43 @@ void leaveTerrine () {
     AbstractLoggable *loggerArray[2]{&lcdLogger, &serialLogger};
     Logger logger("Mega", "LeaveTerrine", LevelLogger::INFO, loggerArray, 1);
 
-
     logger.log("Leave Terrine");
     delay(2000);
+    
   
+    // Distance that is next from the front wall to leave 
+    //  the terrine.
+    static int distFromWall = 5;
+    // Distance that will be add for the next terrine
+    const int distAddEachTime = 5;
 
+
+    turnToObjectiveN(iWest);
+
+    // Going untill the terrine zone to leave it
+    logger.log("Back N cm");
+    forwardUntilWallN(distFromWall); // TODO: Follow the wall until a blank space
+
+    // Leave the terrine
+    logger.log("Leaving Terrine");
+    delay(1000);
+    upClaw(); // We need to pass over the tube supports (up, out, down)
+    platOut();
+    downClaw();
+    openClaw();
+
+    //Return position
+    logger.log("Returning");
+    delay(1000);
+    clawToStartPoint();
+    backwardUntilNoLeft(iWest);
+    backwardNCm(5, false);
+    
+    turnToObjectiveN(iNorth);
+
+    distFromWall += distAddEachTime;
+
+
+    logger.log("Finish");
+    delay(2000);
 }
