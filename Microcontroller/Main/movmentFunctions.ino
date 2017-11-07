25d0b7d965119b1af97c6aac82c81448b3413d86
Diego Cardozo <diego_a_cardozo@hotmail.com>
2017-11-07 16:51:11 -0600

    Changed name of formulaBackwardWithLeftWall to formulaFollowWithWall

--- Microcontroller/Main/movmentFunctions.ino
+++ Microcontroller/Main/movmentFunctions.ino
@@ -963,10 +963,10 @@ int backwardWithLeftWall(const int& degreesObjetivo, const int& objetivoDistPare
   const int diffSharps = distFront - distBack; // (-) voltear a la der
   const int diffCompass = getAngleDifferenceD(degreesObjetivo, getCompass()); // (-) conviene voltear a la derecha
 
-  int velLF = formulaBackwardWithLeftWall(baseLF, -diffSeparacion, diffSharps, diffCompass);
-  int velLB = formulaBackwardWithLeftWall(baseLB, -diffSeparacion, diffSharps, diffCompass);
-  int velRF = formulaBackwardWithLeftWall(baseRF, diffSeparacion, -diffSharps, -diffCompass);
-  int velRB = formulaBackwardWithLeftWall(baseRB, diffSeparacion, -diffSharps, -diffCompass);
+  int velLF = formulaFollowWithWall(baseLF, -diffSeparacion, diffSharps, diffCompass);
+  int velLB = formulaFollowWithWall(baseLB, -diffSeparacion, diffSharps, diffCompass);
+  int velRF = formulaFollowWithWall(baseRF, diffSeparacion, -diffSharps, -diffCompass);
+  int velRB = formulaFollowWithWall(baseRB, diffSeparacion, -diffSharps, -diffCompass);
 
   backward(velLF, velLB, velRF, velRB); // TODO: Use better setVelocity
 
@@ -975,10 +975,10 @@ int backwardWithLeftWall(const int& degreesObjetivo, const int& objetivoDistPare
 
 /**
  * Formula with the P taking the base, distance from the wall, difference from sharps
- * and diff from compass. Used in the backwardWithLeftWall.
+ * and diff from compass. Used in any backward/forwardWithLeft/RightWall.
  *
  */
-int formulaBackwardWithLeftWall(int base, int separacion, int diffBetweenSharps, int diffCompass) {
+int formulaFollowWithWall(int base, int separacion, int diffBetweenSharps, int diffCompass) {
   if (diffBetweenSharps > 45) diffBetweenSharps = 45;
   else if (diffBetweenSharps < -45) diffBetweenSharps = -45;
 //  return base + separacion * 8 + diffBetweenSharps * 2 + diffCompass * 6;
