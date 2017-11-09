2d62c13e71d900017df3d70e77901ada4ee79c49
chaveze <chaaveze@gmail.com>
2017-11-09 03:20:55 +0000

    fixed function

--- Raspberrypi/lib/Larc_vision_2017.py
+++ Raspberrypi/lib/Larc_vision_2017.py
@@ -55,7 +55,7 @@ whiteFrame = np.ones((480,640), np.uint8)
 whiteFrame[:] = 255
 
 #For debugging
-debugger = True
+debugger = False
 
 # Simple class to manage individual squares in the image
 # ATRIBUTES:
