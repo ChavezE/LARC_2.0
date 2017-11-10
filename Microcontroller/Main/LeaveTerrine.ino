04c00940baa7f09d3ae4baa407d62e913ad6f9eb
Diego Cardozo <diego_a_cardozo@hotmail.com>
2017-11-10 09:47:55 -0600

    Implemented leaveTerrineRight

--- Microcontroller/Main/LeaveTerrine.ino
+++ Microcontroller/Main/LeaveTerrine.ino
@@ -112,4 +112,63 @@ void leaveTerrineLeft () {
  * west.
  */
  void leaveTerrineRight () {
+    SerialLog serialLogger;
+    //serialLogger.init();
+    LCDLogger lcdLogger;
+    lcdLogger.init();
+  
+    AbstractLoggable *loggerArray[2]{&lcdLogger, &serialLogger};
+    Logger logger("Mega", "LeaveTerrRight", LevelLogger::INFO, loggerArray, 1);
+
+    logger.log("Leave Terr Right");
+    delay(2000);
+    
+  
+    // Distance that is next from the front wall to leave 
+    //  the terrine.
+    static int distFromWall = 6; // TODO: Maybe this two variables can be global to the both version of the functions
+    // Distance that will be add for the next terrine
+    const int distAddEachTime = 8;
+
+
+    turnToObjectiveN(iWest);
+    // Pass the gate
+    backwardNCm(50, false); // TODO: Maybe is better to pass until pass the sharpLeftBack
+
+    // Going until the terrine zone to leave it
+    logger.log("Back with Left");
+    delay(1000);
+    int distFront, distBack;
+    do {
+        backwardWithLeftWall(iWest, 17, false, distFront, distBack);
+    } while (distFront < 30);
+    brake();
+
+    turnToObjectiveN(iWest); // TODO: It is good idea to check, after turn, the sharpLeftBack if it is very far to return and get closer
+    logger.log("Forw until Cms");
+    delay(1000);
+    backwardUntilWallN(distFromWall);
+
+    // Leave the terrine
+    logger.log("Leaving Terrine");
+    delay(1000);
+    upClaw(); // We need to pass over the tube supports (up, out, down)
+    platOut();
+    downClaw();
+    openClaw();
+
+    //Return position
+    logger.log("Returning");
+    delay(1000);
+    clawToStartPoint();
+    forwardNCm(60, false);
+    forwardUntilNoLeft(iWest);
+    forwardNCm(5, false);
+    
+
+    distFromWall += distAddEachTime;
+
+
+    logger.log("Finish");
+    delay(2000);
  }
